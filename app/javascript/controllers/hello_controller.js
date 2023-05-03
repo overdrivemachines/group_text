60ac2c44d2f714640d212d82a7116af70ef545be
get.dipen@gmail.com <get.dipen@gmail.com>
2023-05-02 22:45:51 -0700

    loading messages using stimulus

--- app/javascript/controllers/hello_controller.js
+++ app/javascript/controllers/hello_controller.js
@@ -1,7 +1,23 @@
-import { Controller } from "@hotwired/stimulus"
+import { Controller } from "@hotwired/stimulus";
 
 export default class extends Controller {
+  static targets = ["btnTemplate1", "btnTemplate2", "txtAreaMessage"];
   connect() {
-    this.element.textContent = "Hello World!"
+    console.log("connected!!!!!");
+    // this.element.textContent = "Hello World!";
+
+    // this.btnTemplate1Target.textContent = "Hello";
+  }
+
+  loadMessage1(event) {
+    event.preventDefault();
+    console.log("hello1");
+    this.txtAreaMessageTarget.value = "This is Jillian with Sierra Geotech...";
+  }
+
+  loadMessage2(event) {
+    event.preventDefault();
+    console.log("hello2");
+    this.txtAreaMessageTarget.value = "Following up to see if any inspections are needed tomorrow";
   }
 }
